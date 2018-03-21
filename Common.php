c03c15aee9bb01eb2e8673d4e322bb3bb1feac82
Takanori Izutani <takanoriizutani@TakanorinoMacBook-Pro.local>
2018-03-21 12:31:28 +0900

    記事一覧、管理側記事一覧、修正画面、削除機能完成

---
+++ Common.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Sample;
+
+class Common
+{
+  const BLOG_TITLE = 'Your Blog Title';
+  const ADMIN_BLOG_TITLE = 'Admin Your Blog Title';
+  /*
+  define('BLOG_TITLE', 'Your Blog Title');
+  define('ADMIN_BLOG_TITLE', 'Admin Your Blog Title');
+
+  define('DEFAULT_ENCODE', 'UTF-8');
+  define('DOMAIN', 'http://localhost:8000/');
+  */
+}
+?>
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
-
+require_once __DIR__.'/Common.php';
+use Symfony\Component\HttpFoundation\Request;
 $app = new Silex\Application();
 
 $loader = new Twig_Loader_Filesystem(__DIR__. '/views');
 $twig = new Twig_Environment($loader);
-$template = $twig->loadTemplate("hello.twig");
 
 ORM::configure('mysql:host=127.0.0.1;dbname=blog');
 ORM::configure('username', 'root');
@@ -20,13 +20,83 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-$app->get('/hello/{name}', function($name) use($template) {
+//記事一覧ページ
+$app->get('/', function() use($twig) {
   #$airticles = ORM::for_table('airticle')->find_many();
   #$user = ORM::for_table('user')->find_many();
   #return $template->render(['user' => $user]);
-  $airticles = ORM::for_table('airticle')->find_many();
-  return $template->render(['airticle' => $airticles]);
+  $articles = ORM::for_table('airticle')->find_many();
+
+  $template = $twig->loadTemplate("index.twig");
+  // var_dump($articles);
+
+  return $template->render(['articles' => $articles]);
 });
+
+//記事詳細
+$app->get('/articles/{id}', function($id) use($twig) {
+  $template = $twig->loadTemplate("article.twig");
+  $article = ORM::for_table('airticle')->where('id', $id)->find_one();
+  return $template->render(['article' => $article]);
+});
+
+//ログイン画面
+$app->get('/login/', function() use($twig) {
+  $template = $twig->loadTemplate("login.twig");
+  return $template->render([]);
+});
+
+$app->post('/login/', function() use($twig){
+  count(ORM::for_table('airticle')->find_result_set());
+
+});
+
+//管理側記事一覧
+$app->get('/admin/', function() use($twig) {
+  //die("x");
+  $template = $twig->loadTemplate("admin.twig");
+  //die("a");
+   $articles = ORM::for_table('airticle')->find_many();
+  // var_dump($articles);die();
+  return $template->render(['articles' => $articles]);
+});
+
+//管理側削除
+$app->get('/delete/{id}', function($id) use($twig, $app) {
+  #die("x");
+  $template = $twig->loadTemplate("admin.twig");
+  #die("a");
+
+  $record = ORM::for_table('airticle')->where('id', $id)->find_one();
+  $record->delete();
+  return $app->redirect('/admin/');
+});
+
+//編集画面
+$app->get('/edit/{id}', function($id) use($twig) {
+  #die("x");
+  $template = $twig->loadTemplate("edit.twig");
+  #die("a");
+
+  $article = ORM::for_table('airticle')->where('id', $id)->find_one();
+  return $template->render(['article' => $article]);
+});
+
+//編集処理
+$app->post('/edit/{id}', function($id, Request $request) use($twig, $app){
+#$app->post('/edit/{id}', function($id) use($twig, $app){
+#  die("post");
+  $template = $twig->loadTemplate("edit.twig");
+  $title = $request->get('title');
+  $body = $request->get('body');
+  #die("iii");
+  $update = ORM::for_table('airticle')->where('id', $id)->find_one()
+  ->set('title', $title)
+  ->set('body', $body);
+  $update->save();
+  return $app->redirect('/edit/' . $id);
+});
+
 
 $app->run();
 ?>
